14d42acb79414bb66861079adefa3bb176160819
lpbborges <luizpaulobborges@gmail.com>
2023-08-21 20:08:29 -0300

    hacker_rank: miniMaxSum

---
+++ js/hacker_rank/miniMaxSum.js
@@ -0,0 +1,16 @@
+function miniMaxSum(arr) {
+    // Write your code here
+    const sortedArr = arr.sort();
+
+    const {min, max} = sortedArr.reduce((sumObj, curr, idx) => {
+        return {
+            min: idx < sortedArr.length - 1  ? sumObj.min + curr : sumObj.min, 
+            max: idx > 0 ? sumObj.max + curr : sumObj.max
+
+        };
+    }, 
+        {min: 0, max: 0}
+    );
+
+    console.log(`${min} ${max}`);
+}
