a1e9db2fa530f5e260675db7951ff7c8eb95cc15
lpbborges <luizpaulobborges@gmail.com>
2023-08-28 19:39:17 -0300

    hacker_rank: diagonalDifference

---
+++ js/hacker_rank/diagonalDifference.js
@@ -0,0 +1,18 @@
+function diagonalDifference(arr) {
+    let leftToRighDiagonal = 0;
+    let rightToLeftDiagonal = 0;
+    const lastIndex = arr.length - 1;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (i === j) {
+                leftToRighDiagonal += arr[i][j];
+            }
+            if (j === lastIndex - i) {
+                rightToLeftDiagonal += arr[i][j];
+            }
+        }
+    }
+
+    return Math.abs(leftToRighDiagonal - rightToLeftDiagonal);
+}
