20bf380163b872ccab68ebdc8da608051e9637fe
lpbborges <luizpaulobborges@gmail.com>
2023-09-02 16:56:27 -0300

    hacker_rank: countingSort

---
+++ js/hacker_rank/countingSort.js
@@ -0,0 +1,11 @@
+function countingSort(arr) {
+    const countingArr = Array.from({ length: 100 }).fill(0);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 100) {
+            countingArr[arr[i]] += 1;
+        }
+    }
+
+    return countingArr;
+}
